82649288a7c2ef4509819d09ba1fb48be65edc07
Muhammed ÇELİK <developerhost01@gmail.com>
2024-09-01 17:35:43 +0300

    Updated

--- dev/_static/scripts/version-switcher.js
+++ dev/_static/scripts/version-switcher.js
@@ -22,7 +22,7 @@ function addVersionSwitcherAvailDocsLink() {
               var availDocsLink = document.createElement("a");
               availDocsLink.setAttribute(
                 "href",
-                "https://scikit-learn.org/dev/versions.html"
+                "https://scikit-plots.github.io/dev/versions.html"
               );
               availDocsLink.innerHTML = "More";
               // We use the same class as the last entry to be safe
