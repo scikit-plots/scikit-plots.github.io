4e938189a49a53f79ff0170a5c9cc95c7b4f9e87
scikit-plots <ci@scikit-plots.github.io>
2025-05-05 15:36:48 +0000

    Pushing the docs to dev/ for branch: main, commit f5d58d03c15218dc764684bc9b5568687336699d

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.5.0.dev0+git.20250430.079ba10',
+    VERSION: '0.5.0.dev0+git.20250505.f5d58d0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
